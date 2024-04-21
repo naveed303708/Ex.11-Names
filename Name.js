c7f421c5b894dca902ac9de21ae8a4752a0b1a80
Naveed Ahmed Farooqui <162836180+naveed303708@users.noreply.github.com>
2024-04-21 23:29:24 +0500

    Add files via upload

---
+++ Name.js
@@ -0,0 +1,5 @@
+"use strict";
+let names = ['Naveed', 'Ahmed', 'Farooqi'];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Name.ts
@@ -0,0 +1,9 @@
+
+
+let names :string[] = ['Naveed','Ahmed','Farooqi'] ;
+
+for(let i=0; i<names.length; i++){
+
+    console.log(names[i]);
+
+} 
